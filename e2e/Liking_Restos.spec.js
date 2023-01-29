b5d0c9c7f5349f1ebd4c84491ee9714f5c594e64
Hfn <titocs845@gmail.com>
2023-01-29 11:35:43 +0700

    fixing end to end test

--- e2e/Liking_Restos.spec.js
+++ e2e/Liking_Restos.spec.js
@@ -4,25 +4,27 @@ Feature('Favoriting Restos');
 const assert = require('assert');
 
 Before(({ I }) => {
-  I.amOnPage('/#/resto-favorite');
+  I.amOnPage('/#/resto-favorit');
 });
 
 const favoriteEmptyMessage = 'You dont have any favorite restaurant';
 
 Scenario('Favoriting one restaurant', async ({ I }) => {
+  I.waitForElement('.error-message', 5);
   I.see(favoriteEmptyMessage, '.error-message');
 
   I.amOnPage('/');
-
+  I.waitForElement('.resto-image__title > p', 5);
   I.seeElement('.resto-image__title > p');
   const firstCard = locate('.resto-image__title > p').first();
   const firstCardTitle = await I.grabTextFrom(firstCard);
   I.click(firstCard);
 
+  I.waitForElement('#likeButton', 5);
   I.seeElement('#likeButton');
   I.click('#likeButton');
 
-  I.amOnPage('/#/resto-favorite');
+  I.amOnPage('/#/resto-favorit');
   I.seeElement('.resto-wrapper__item');
   const likedCardTitle = await I.grabTextFrom('.resto-image__title > p');
 
@@ -30,30 +32,33 @@ Scenario('Favoriting one restaurant', async ({ I }) => {
 });
 
 Scenario('unFavoriting one restaurant', async ({ I }) => {
+  I.waitForElement('.error-message', 5);
   I.see(favoriteEmptyMessage, '.error-message');
 
   I.amOnPage('/');
+  I.waitForElement('.resto-image__title > p', 5);
 
   I.seeElement('.resto-image__title > p');
   const firstCard = locate('.resto-image__title > p').first();
   const firstCardTitle = await I.grabTextFrom(firstCard);
   I.click(firstCard);
 
+  I.waitForElement('#likeButton', 5);
   I.seeElement('#likeButton');
   I.click('#likeButton');
 
-  I.amOnPage('/#/resto-favorite');
+  I.amOnPage('/#/resto-favorit');
   I.seeElement('.resto-wrapper__item');
   const likedCardTitle = await I.grabTextFrom('.resto-image__title > p');
 
   assert.strictEqual(firstCardTitle, likedCardTitle);
 
   I.click(likedCardTitle);
+  I.waitForElement('#likedButton', 5);
+  I.seeElement('#likedButton');
+  I.click('#likedButton');
 
-  I.seeElement('#likeButton');
-  I.click('#likeButton');
-
-  I.amOnPage('/#/resto-favorite');
+  I.amOnPage('/#/resto-favorit');
   I.seeElement('.error-message');
   const noFavoriteRestaurant = await I.grabTextFrom('.error-message');
 
